8ab6a59392c367cffa10071b9aed71b6ba961c6f
Azamat <azamatk9799@gmail.com>
2025-02-19 23:01:19 +0500

    feat: add users module

---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  getByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } })
+  }
+}
