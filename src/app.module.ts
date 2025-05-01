eae54efeb4cfac9b77c2cdf33290003edf0bd8f5
Azamat <azamatk9799@gmail.com>
2025-05-01 22:02:20 +0500

    chore: settings app module and main file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,86 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { AuthModule } from './auth/auth.module'
-import { UsersModule } from './users/users.module'
+import { Logger, Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
-import { PrismaModule } from 'src/prisma/prisma.module'
-import { LoggerMiddleware } from 'src/common/logger/logger.middleware'
-import { StudentsModule } from './students/students.module'
-import { ManagerModule } from './manager/manager.module';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n'
+import * as path from 'path'
+import { PrismaModule } from 'src/common/services/prisma/prisma.module'
+import { TranslationModule } from 'src/common/services/translation/translation.module'
+import { AuthModule } from 'src/modules/auth/auth.module'
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard'
+import { DeleteReasonModule } from 'src/modules/delete-reason/delete-reason.module'
+import { GroupModule } from 'src/modules/group/group.module'
+import { PaymentMethodModule } from 'src/modules/payment-method/payment-method.module'
+import { PaymentsModule } from 'src/modules/payments/payments.module'
+import { StudentsModule } from 'src/modules/students/students.module'
+import { UsersModule } from 'src/modules/users/users.module'
+import configuration, { AppConfig, LoggerConfig, LoggerFormat } from 'src/config/configuration'
+import { LoggerModule } from 'nestjs-pino'
+import { FileModule } from 'src/common/services/file/file.module'
+import { CourseModule } from 'src/modules/course/course.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    I18nModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService<AppConfig>) => ({
+        fallbackLanguage: config.get('defaultLanguage') as string,
+        loaderOptions: {
+          path: path.join(__dirname, '/i18n/'),
+          watch: true,
+        },
+        typesOutputPath: path.join(process.cwd(), 'src/common/types/i18n.generated.ts'),
+      }),
+      resolvers: [new HeaderResolver(['x-lang']), new QueryResolver(['lang']), new AcceptLanguageResolver()],
+    }),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService<AppConfig>) => {
+        const loggerConfig = config.get<LoggerConfig>('logger')
+
+        return {
+          pinoHttp: {
+            level: loggerConfig?.level,
+            transport: loggerConfig?.format === LoggerFormat.Pretty ? { target: 'pino-pretty' } : undefined,
+            useLevelLabels: true,
+            formatters: {
+              level: (label: string) => {
+                return { level: label }
+              },
+            },
+            autoLogging: false,
+          },
+        }
+      },
     }),
     AuthModule,
     UsersModule,
     PrismaModule,
     StudentsModule,
-    ManagerModule,
+    TranslationModule,
+    FileModule,
+    PaymentsModule,
+    PaymentMethodModule,
+    DeleteReasonModule,
+    GroupModule,
+    CourseModule,
   ],
-  controllers: [],
   providers: [
+    Logger,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
     },
   ],
-  exports: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*')
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,71 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
-import { json, urlencoded } from 'express'
+import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n'
 import multer from 'multer'
+import { ConfigService } from '@nestjs/config'
+import { AppConfig } from 'src/config/configuration'
+import { Logger } from 'nestjs-pino'
+import { VersioningType } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { PrismaModel } from 'prisma/generated/classes'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    snapshot: true,
+  })
 
-  app.use(json())
-  app.use(urlencoded({ extended: true }))
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
-  app.use(multer().any())
+  const configService = app.get<ConfigService<AppConfig>>(ConfigService)
+  const port = configService.get<number>('port')
+  const logger = app.get(Logger)
+  const corsMaxAge = configService.get<number>('corsMaxAge')
+
+  app.useLogger(app.get(Logger))
+  app.enableCors({
+    maxAge: corsMaxAge,
+  })
+  app.enableVersioning({
+    type: VersioningType.URI,
+  })
+  app.setGlobalPrefix('api')
   app.enableShutdownHooks()
-  await app.listen(process.env.PORT ?? 3000)
+  app.use(multer().any())
+
+  app.useGlobalPipes(
+    new I18nValidationPipe({
+      transform: true,
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  )
+
+  app.useGlobalFilters(
+    new I18nValidationExceptionFilter({
+      detailedErrors: false,
+    }),
+  )
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('Maqsad LMS API')
+    .setDescription('API для управления курсами, пользователями и ролями в Maqsad LMS')
+    .setVersion('1.0')
+    .addServer(`http://localhost:${port}`, 'Local')
+    .addServer(`https://api.maqsad.com`, 'Production')
+    .addBearerAuth()
+    .build()
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, openApiConfig, {
+      deepScanRoutes: true,
+      extraModels: [...PrismaModel.extraModels],
+    })
+  SwaggerModule.setup('api-docs', app, documentFactory, {
+    jsonDocumentUrl: 'api-docs/json',
+    raw: ['json'],
+  })
+
+  await app.listen(port ?? 3001, () => {
+    logger.log(`Application started at port:${port}`)
+  })
 }
 bootstrap()
